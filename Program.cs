521587132e0f3c28bbdcab30273166ea0dcd239a
Danilo Alves <daniloalves.1996@gmail.com>
2022-12-02 19:46:00 -0300

    criado comentario da atividade

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace modulo_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Atividade:
+             * Crie um programa que receba dois valores e calcule a soma desses valores.
+             * Ao final exiba o resultado no console.
+             * 
+             */
+        }
+    }
+}
