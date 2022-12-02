f30aae565d59e6234ea6a9da6dbcd3bcbab97417
Danilo Alves <daniloalves.1996@gmail.com>
2022-12-02 20:55:48 -0300

    metodo soma

--- Program.cs
+++ Program.cs
@@ -11,6 +11,27 @@ namespace modulo_Git
              * Ao final exiba o resultado no console.
              * 
              */
+            #region Base
+            Console.WriteLine("Digite 1 para iniciar o programa ou digite 0 para sair");
+            int opcao = int.Parse(Console.ReadLine());
+            while(opcao !=0) 
+            {
+                int a, b;
+                Console.WriteLine("informa o valor dos numeros a serem somados");
+                a = int.Parse(Console.ReadLine());
+                b = int.Parse(Console.ReadLine());
+                int resultado = Soma(a + b);
+                Console.WriteLine("a soma dos  valores e igual a " + resultado);
+                Console.WriteLine("se deseja repetir digite 1 se nao digite 0");
+                opcao = int.Parse(Console.ReadLine());
+
+            }
+            #endregion
+
+        }
+        static int Soma(int a, int b)
+        {
+            return a + b;
         }
     }
 }
